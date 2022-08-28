f18068e5067cda4d7e8bf12df344cda5bb6a7ad3
hwdmedia <hwdmedia@m5.boomstream.com>
2022-08-28 11:02:59 +0300

    +10 jibri devices

--- Boomstream.mk
+++ Boomstream.mk
@@ -1,5 +1,11 @@
 DOCKER_COMPOSE?=docker-compose
 
+start-single: docker-compose.yml jibri.yml
+	$(DOCKER_COMPOSE) -f docker-compose.yml -f jibri.yml up -d
+
+stop-single:
+	$(DOCKER_COMPOSE) -f docker-compose.yml -f jibri.yml down -v
+
 start: docker-compose.yml jibri_multi.yml
 	$(DOCKER_COMPOSE) -f docker-compose.yml -f jibri_multi.yml up -d
 
